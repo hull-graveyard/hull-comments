361a09b37daee97152850bb8f7d82a6f6518038d
Romain Dardour <romain@hull.io>
2015-03-10 17:03:11 +0100

    use embeds instead of direct boot

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ Hull.init(hullConfig);
 
 var appInit = function(hull, me, platform, org){
 
-  // // When embedded by Hull. this is how the app will be booted:
-  // hull.embed(platform.deployments)
+  // When embedded by Hull. this is how the app will be booted:
+  hull.embed(platform.deployments)
 
   // In development, skip the embed process and start the ship directly
-  App.start(document.getElementById('ship'),platform.deployments[0]);
+  // App.start(document.getElementById('ship'),platform.deployments[0]);
 }
 
 Hull.ready(appInit)
