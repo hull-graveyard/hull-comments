f6b3f8963e6b33d6bb7dd48e83d7db8b41f3d6ae
Romain Dardour <romain@hull.io>
2015-03-11 14:55:47 +0100

    Update embed method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import App from './components/app';
-Hull.init(hullConfig);
 
-var appInit = function(hull, me, platform, org){
-
-  // When embedded by Hull. this is how the app will be booted:
-  hull.embed(platform.deployments)
-
-  // In development, skip the embed process and start the ship directly
-  // App.start(document.getElementById('ship'),platform.deployments[0]);
-}
-
-Hull.ready(appInit)
+Hull.ready(function(hull, me, platform, org){
+  if(platform.type==='ship'){
+    var deployment= { ship:platform, settings: {selector:'#ship'} }
+  } else {
+    var deployment = platform.deployments[0];
+  }
+  App.start(document.querySelector(deployment.settings.selector),deployment);
+})
