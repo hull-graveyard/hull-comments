e6046b5d8f48cd8be6cb7d04c50b6428c370ed6b
Stephane Bellity <sbellity@gmail.com>
2015-09-30 16:16:36 +0200

    Add version and git rev to gh-pages commit message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import App from './components/app';
-// import en from '../locales/en.json';
 
 Hull.ready(function(hull, me, platform, org) {
   var d = platform.deployments[0];
-  // d.ship.translations.en = en;
   App.start(document.querySelector('#ship'), d, Hull);
 });
